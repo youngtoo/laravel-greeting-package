82b7de0cac311ec959f03662ed0400cee23d9b07
Ian Too <ianktoo@gmail.com>
2024-08-13 15:42:43 +0300

    Added scr files to repo

---
+++ src/GreetingServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Iantoo\GreetingPackage;
+
+use Illiminate\Support\ServiceProvider;
+
+class GreetingServiceProvider extends ServiceProvider
+{
+    public function register(){
+        // Register bindings.
+        
+    }
+
+    public function boot(){
+        // Publish resources on perform actions on package load.
+
+    }
+}
